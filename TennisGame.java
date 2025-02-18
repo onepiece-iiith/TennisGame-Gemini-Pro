9ac6c28f54f412b146e66bd82e9ba12b416a7814
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-18 14:34:44 +0530

    Refactored code to address design smells

--- TennisGame.java
+++ TennisGame.java
@@ -1,14 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
-    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Map<Integer, String> scoreMap = new HashMap<>();
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        scoreMap.put(0, "Love");
+        scoreMap.put(1, "Fifteen");
+        scoreMap.put(2, "Thirty");
+        scoreMap.put(3, "Forty");
     }
 
     public void wonPoint(String playerName) {
@@ -19,58 +25,38 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1 == m_score2) {
+            return getEqualScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getAdvantageOrWinScore();
+        } else {
+            return getRunningScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private String getEqualScore() {
+        if (m_score1 < 3) {
+            return scoreMap.get(m_score1) + "-All";
+        } else {
+            return "Deuce";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private String getAdvantageOrWinScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
-        return score;
+    }
+
+    private String getRunningScore() {
+        return scoreMap.get(m_score1) + "-" + scoreMap.get(m_score2);
+
     }
 }
