d8b3064d735701581e77ead19a30c17eeceacb04
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-18 14:51:12 +0530

    Refactored code to address design smells

--- TennisGame.java
+++ TennisGame.java
@@ -1,6 +1,5 @@
-
 public class TennisGame1 implements TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -37,7 +36,6 @@ public class TennisGame1 implements TennisGame {
                 default:
                         score = "Deuce";
                     break;
-                
             }
         }
         else if (m_score1>=4 || m_score2>=4)
@@ -53,24 +51,27 @@ public class TennisGame1 implements TennisGame {
             for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
+                else { score+="-"; tempScore = m_score2;}                switch(tempScore)
                 {
                     case 0:
-                        score+="Love";
+                        score+= "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score+= "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score+= "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score+= "Forty";
                         break;
                 }
             }
         }
         return score;
     }
+
+    // Added getter methods.  Crucial for decoupling. 
+    public int getPlayer1Score(){return m_score1;}
+    public int getPlayer2Score(){return m_score2;}
 }
